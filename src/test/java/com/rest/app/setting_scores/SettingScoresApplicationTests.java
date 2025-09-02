75fd0d7bdfc695ba7ea227a09afba03b6d109421
Omar Zhadykov <omar3282088@gmail.com>
2025-09-02 22:15:06 +0200

    init

---
+++ src/test/java/com/rest/app/setting_scores/SettingScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.app.setting_scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
