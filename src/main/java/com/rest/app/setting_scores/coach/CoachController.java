22cd61086644b567c5163d1f9dc17349982f7aa7
Omar Zhadykov <omar3282088@gmail.com>
2025-09-14 16:32:48 +0200

    tried java bean lifecycle methods

--- src/main/java/com/rest/app/setting_scores/coach/CoachController.java
+++ src/main/java/com/rest/app/setting_scores/coach/CoachController.java
@@ -10,26 +10,18 @@ public class CoachController {
 
     // define a private field for the depepndency
     private Coach myCoach;
-    private Coach anotherCoach;
 
     // define a constructor for dependency injection
     @Autowired
     public CoachController(
-            @Qualifier("cricketCoach") Coach coach,
-            @Qualifier("cricketCoach") Coach anotherCoach
+            @Qualifier("cricketCoach") Coach coach
     ) {
         System.out.println("in constructor: " + getClass().getSimpleName());
         myCoach = coach;
-        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String dailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Camparing beans: maCoach == anotherCoach " + (myCoach == anotherCoach);
     }
 }
--- src/main/java/com/rest/app/setting_scores/coach/CricketCoach.java
+++ src/main/java/com/rest/app/setting_scores/coach/CricketCoach.java
@@ -1,14 +1,27 @@
 package com.rest.app.setting_scores.coach;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
     public CricketCoach() {
         System.out.println("Cricket Coach Initialzing");
+    }
+
+    // init method
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Cricket Coach initializing");
+    }
+
+    // destroy method
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("Cricket Coach destroying");
     }
 
     @Override
